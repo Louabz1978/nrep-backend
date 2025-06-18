ac81786b211690e097bd33db844f314df76f2d53
Lou Abouzaid <louabouzaid@Lous-MBP.lan>
2025-06-18 16:09:53 -0500

    creating auth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routers import users , properties
+from app.routers import users , properties, auth
 from app.database import engine, Base
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -14,6 +14,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(properties.router)
 
