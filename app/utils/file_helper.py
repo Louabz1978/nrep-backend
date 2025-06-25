0c0bc83adf8f6c73bc095598685adb4e56745d9a
Lou Abouzaid <louabouzaid@Lous-MBP.lan>
2025-06-25 15:17:17 -0500

    fixed file_helper

--- app/utils/file_helper.py
+++ app/utils/file_helper.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
 def load_sql(filename: str) -> str:
-    sql_path = Path(__file__).parent.parent / "sql" / filename
+    sql_path = Path(__file__).parent.parent / "sql_queries" / filename
     return sql_path.read_text()
