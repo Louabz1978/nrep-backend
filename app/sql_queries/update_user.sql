c56b68c5e7c22dc75c06715446c595ad5602efb3
Rukaia Alabrash <rukaiaalabrash@gmail.com>
2025-06-26 18:27:39 +0300

    update_user

---
+++ app/sql_queries/update_user.sql
@@ -0,0 +1,29 @@
+UPDATE users
+SET
+    first_name = :first_name,
+    last_name = :last_name,
+    email = :email,
+    password_hash = :password_hash,
+    role = :role,
+    phone_number = :phone_number,
+    address = :address,
+    neighborhood = :neighborhood,
+    city = :city,
+    county = :county,
+    lic_num = :lic_num,
+    agency_id = :agency_id
+WHERE user_id = :user_id;UPDATE users
+SET
+    first_name = :first_name,
+    last_name = :last_name,
+    email = :email,
+    password_hash = :password_hash,
+    role = :role,
+    phone_number = :phone_number,
+    address = :address,
+    neighborhood = :neighborhood,
+    city = :city,
+    county = :county,
+    lic_num = :lic_num,
+    agency_id = :agency_id
+WHERE user_id = :user_id;
