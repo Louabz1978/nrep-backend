398f577b320c2c086178815e072fe56c294290a0
Rukaia Alabrash <rukaiaalabrash@gmail.com>
2025-07-13 09:17:38 +0300

    correct roles_out.py

--- app/models/role_model.py
+++ app/models/role_model.py
@@ -1,18 +1,19 @@
 from sqlalchemy import String, Integer, Float, ForeignKey, Date, TIMESTAMP, Text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.database import Base
+from sqlalchemy import Boolean
 
 class Role(Base):
     __tablename__ = "roles"
     
     roles_id: Mapped[int] = mapped_column(primary_key=True, index=True, autoincrement=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.user_id", ondelete="CASCADE"), nullable=False)
-    admin: Mapped[bool] = mapped_column(bool)
-    broker: Mapped[bool] = mapped_column(bool)
-    realtor: Mapped[bool] = mapped_column(bool)
-    buyer: Mapped[bool] = mapped_column(bool)
-    seller: Mapped[bool] = mapped_column(bool)
-    tenant: Mapped[bool] = mapped_column(bool)
+    admin: Mapped[bool] = mapped_column(Boolean)
+    broker: Mapped[bool] = mapped_column(Boolean)
+    realtor: Mapped[bool] = mapped_column(Boolean)
+    buyer: Mapped[bool] = mapped_column(Boolean)
+    seller: Mapped[bool] = mapped_column(Boolean)
+    tenant: Mapped[bool] = mapped_column(Boolean)
     
     #Relationships
     user = relationship("User", back_populates="roles", foreign_keys=[user_id])
