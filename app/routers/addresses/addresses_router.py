870e1c90d4be82877c34bfab5d59490eee83c329
ayla <alraasayla@gmail.com>
2025-07-21 19:23:08 +0300

    update address by id endpoint

--- app/routers/addresses/addresses_router.py
+++ app/routers/addresses/addresses_router.py
@@ -15,73 +15,6 @@ router = APIRouter(
     tags=["addresses"]
 )
 
-@router.put("/address")
-def update_address(
-    address : AddressUpdate,
-    db : Session = Depends(database.get_db),
-    current_user : User = Depends(get_current_user)
-):
-    sql = load_sql("user/get_user_by_id.sql")
-    user_data = db.execute(text(sql), {"user_id": current_user.user_id}).mappings().first()
-
-    if not user_data:
-        raise HTTPException(status_code=404, detail="user not found")
-    
-    get_sql = load_sql("address/get_address_by_id.sql")
-    address_data = db.execute(text(get_sql), {"address_id":user_data["address_id"]}).mappings().first()
-
-    if not address_data:
-        raise HTTPException(status_code=404, detail="address not found")
-    
-    #update address
-    db_address = address.model_dump(exclude_unset=True)
-    db_address["address_id"] = user_data["address_id"]
-    set_clause = ", ".join(f"{k} = :{k}" for k in db_address)
-    sql = f"UPDATE ADDRESSES SET {set_clause} WHERE address_id = :address_id RETURNING address_id;"
-    updated_address_id = db.execute(text(sql), db_address).scalar()
-    db.commit()
-
-    #fetch address data
-    updated_address = db.execute(text(get_sql),{"address_id":updated_address_id}).mappings().first()
-    address_details = AddressOut(**updated_address)
-
-    return {"message" : "address updated successfully", "address" : address_details}
-
-@router.get("", status_code=status.HTTP_200_OK)
-def get_address(
-    db: Session = Depends(database.get_db),
-    current_user: User = Depends(get_current_user)
-):
-    sql = load_sql("address/get_address.sql")
-    result = db.execute(text(sql), {'address_id': current_user.address_id})
-    row = result.mappings().first()
-
-    if row is None:
-        raise HTTPException(status_code=404, detail="Address not found")
-    
-    address = AddressOut(**row)
-    return address
-
-@router.delete("/{address_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_address(
-    address_id: int,
-    db: Session = Depends(database.get_db),
-    current_user: User = Depends(get_current_user)
-):
-    if not current_user.roles.admin:
-        raise HTTPException(status_code=403, detail="Not authorized")
-
-    sql = load_sql("address/get_address_by_id.sql")
-    address = db.execute(text(sql), {"address_id": address_id}).mappings().first()
-    if not address:
-        raise HTTPException(status_code=404, detail="Address not found")
-
-    delete_sql = load_sql("address/delete_address.sql")
-    db.execute(text(delete_sql), {"address_id": address_id})
-    
-    db.commit()
-    return {"message": "Address deleted successfully"}
-
 
 @router.get("", status_code=status.HTTP_200_OK)
 def get_address(
@@ -116,3 +49,101 @@ def get_address_by_id(
     
     address = AddressOut(**row)
     return address
+
+
+@router.put("")
+def update_address(
+    address : AddressUpdate,
+    db : Session = Depends(database.get_db),
+    current_user : User = Depends(get_current_user)
+):
+    sql = load_sql("user/get_user_by_id.sql")
+    user_data = db.execute(text(sql), {"user_id": current_user.user_id}).mappings().first()
+
+    if not user_data:
+        raise HTTPException(status_code=404, detail="user not found")
+    
+    get_sql = load_sql("address/get_address_by_id.sql")
+    address_data = db.execute(text(get_sql), {"address_id":user_data["address_id"]}).mappings().first()
+
+    if not address_data:
+        raise HTTPException(status_code=404, detail="address not found")
+    
+    #update address
+    db_address = address.model_dump(exclude_unset=True)
+    db_address["address_id"] = user_data["address_id"]
+    set_clause = ", ".join(f"{k} = :{k}" for k in db_address)
+    sql = f"UPDATE ADDRESSES SET {set_clause} WHERE address_id = :address_id RETURNING address_id;"
+    updated_address_id = db.execute(text(sql), db_address).scalar()
+    db.commit()
+
+    #fetch address data
+    updated_address = db.execute(text(get_sql),{"address_id":updated_address_id}).mappings().first()
+    address_details = AddressOut(**updated_address)
+
+    return {"message" : "address updated successfully", "address" : address_details}
+
+
+
+@router.put("/{address_id}")
+def update_address_by_id(
+    address_id: int,
+    address: AddressUpdate,
+    db: Session = Depends(database.get_db),
+    current_user: User = Depends(get_current_user)
+):
+    
+    #get address and created_by_user data
+    get_sql = load_sql("address/get_address_by_id.sql")
+    address_data = db.execute(text(get_sql), {"address_id": address_id}).mappings().first()
+    sql = load_sql("user/get_user_by_id.sql")
+    created_by_user = db.execute(text(sql), {"user_id": address_data["created_by"]}).mappings().first()
+
+    if not address_data:
+        raise HTTPException(status_code=404, detail="address not found")
+    if not created_by_user:
+        raise HTTPException(status_code=404, detail="creator not found")
+    
+    #Authorization
+    sql = load_sql("role/get_user_roles.sql")
+    user_roles = db.execute(text(sql), {"user_id": current_user.user_id}).mappings().first()
+    if not (
+        user_roles["admin"] == True
+        or (user_roles["broker"] == True and current_user.user_id in (address_data["created_by"], created_by_user["created_by"]))
+        or (user_roles["realtor"] == True and current_user.user_id == address_data["created_by"])
+    ):
+        raise HTTPException(status_code=403, detail="Not authorized")
+    
+    db_address = address.model_dump(exclude_unset=True)
+    db_address["address_id"] = address_id
+    set_clause = ", ".join(f"{k} = :{k}" for k in db_address)
+    sql = f"UPDATE ADDRESSES SET {set_clause} WHERE address_id = :address_id RETURNING address_id;"
+    updated_address_id = db.execute(text(sql), db_address).scalar()
+    db.commit() 
+
+    #fetch address data
+    updated_address = db.execute(text(get_sql),{"address_id":updated_address_id}).mappings().first()
+    address_details = AddressOut(**updated_address)
+
+    return {"message" : "address updated successfully", "address" : address_details}
+
+
+@router.delete("/{address_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_address(
+    address_id: int,
+    db: Session = Depends(database.get_db),
+    current_user: User = Depends(get_current_user)
+):
+    if not current_user.roles.admin:
+        raise HTTPException(status_code=403, detail="Not authorized")
+
+    sql = load_sql("address/get_address_by_id.sql")
+    address = db.execute(text(sql), {"address_id": address_id}).mappings().first()
+    if not address:
+        raise HTTPException(status_code=404, detail="Address not found")
+
+    delete_sql = load_sql("address/delete_address.sql")
+    db.execute(text(delete_sql), {"address_id": address_id})
+    
+    db.commit()
+    return {"message": "Address deleted successfully"}
