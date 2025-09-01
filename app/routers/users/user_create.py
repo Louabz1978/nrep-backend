10da23289a6a622496da14b47ce4945d145c50f5
ayla <alraasayla@gmail.com>
2025-09-01 13:33:32 +0300

    create user api validations

--- app/routers/users/user_create.py
+++ app/routers/users/user_create.py
@@ -1,41 +1,76 @@
-from pydantic import BaseModel, EmailStr, Field, field_validator
-from typing import List
+from pydantic import BaseModel, EmailStr, Field, field_validator, StringConstraints
+from typing import List,Annotated
 import re
 
 from .roles_enum import UserRole
 
-class UserCreate(BaseModel):
-    first_name: str = Field(..., min_length=2, max_length=50)
-    last_name: str = Field(..., min_length=2, max_length=50)
-    email: EmailStr
+name_pattern = re.compile(r'^[A-Za-z\u0600-\u06FF\s]+$')
+phone_number_pattern = re.compile(r'^\d{7,15}$')
 
-    password: str 
-    # password: str = Field(..., min_length=8)
+class UserCreate(BaseModel):
+    first_name: Annotated[
+        str,
+        StringConstraints(strip_whitespace=True, min_length=2, max_length=50),
+        Field(description="First name in Arabic or English, 2–50 chars")
+    ]
+
+    last_name: Annotated[
+        str,
+        StringConstraints(strip_whitespace=True, min_length=2, max_length=50),
+        Field(description="Last name in Arabic or English, 2–50 chars")
+    ]
+
+    email: Annotated[
+        EmailStr,
+        Field(description="Valid email address")
+    ]
+
+    password: str
+    # password: Annotated[
+    #     str,
+    #     StringConstraints(min_length=8),
+    #     Field(description="At least 8 chars, must include an uppercase letter and a digit")
+    # ]
 
     role: List[UserRole]
-    phone_number: str  
+    
+    phone_number: Annotated[
+        str,
+        StringConstraints(strip_whitespace=True),
+        Field(description="Must be a valid phone number")
+    ] 
 
     model_config = {
         "from_attributes": True
     }
     
-    # @validator("password")
+    @field_validator("first_name")
+    def validate_first_name(cls,v: str) -> str:
+        if not name_pattern.match(v):
+            raise ValueError("First name must contain only Arabic or English letters")
+        return v
+    
+    @field_validator("last_name")
+    def validate_last_name(cls,v: str) -> str:
+        if not name_pattern.match(v):
+            raise ValueError("Last name must contain only Arabic or English letters")
+        return v
+
+    # @field_validator("password")
     # def validate_password_strength(cls, v):
-    #     if not re.search(r"\d", v):
-    #         raise ValueError("must contain at least one digit")
-    #     if not re.search(r"[A-Z]", v):
-    #         raise ValueError("must contain at least one uppercase letter")
+    #     if ( not re.search(r"\d", v) ) or ( not re.search(r"[A-Z]",v) ):
+    #         raise ValueError("must contain at least one uppercase letter and one digit")
     #     return v
 
     @field_validator("phone_number")
-    def validate_and_convert_phone(cls, v):
+    def validate_and_convert_phone(cls, v: str) -> str:
         # Optional: remove '+' if exists
         v = v.strip()
         if v.startswith('+'):
             v = v[1:]
 
         # Validate
-        if not re.match(r"^\d{7,15}$", v):
+        if not phone_number_pattern.match(v):
             raise ValueError("must be a valid phone number")
 
-        return int(v)
+        return v
