4aace5e5b4e8b0996aee0fd00b9c923e63486841
Shahd <shahd_abbara@Shahds-MacBook-Air.local>
2025-09-03 16:02:55 +0300

    adding get property by id test cases

--- tests/seed_data.sql
+++ tests/seed_data.sql
@@ -35,10 +35,17 @@ VALUES (5, TRUE, 5);
 
 -- Insert tenant test account
 INSERT INTO users(user_id, first_name, last_name, email, password_hash, phone_number, created_by)
-VALUES (6, 'tenant', 'test', 'test@tenant.com', '$2b$12$2h9HK/lHUQxA.YnNt3qKD.XEscVHESnYPp9LodVOHRNsBZO0CHjZG', '0933111222', 3);
+VALUES (6, 'tenant', 'test', 'test@tenant.com', '$2b$12$2h9HK/lHUQxA.YnNt3qKD.XEscVHESnYPp9LodVOHRNsBZO0CHjZG', '0933111222', 2);
 
 INSERT INTO roles(roles_id, tenant, user_id)
 VALUES (6, TRUE, 6);
+
+INSERT INTO users(user_id, first_name, last_name, email, password_hash, phone_number, created_by)
+VALUES (7, 'noRole', 'test', 'test@norole.com', '$2b$12$2h9HK/lHUQxA.YnNt3qKD.XEscVHESnYPp9LodVOHRNsBZO0CHjZG', '0933111222', 1);
+
+INSERT INTO roles(roles_id,user_id)
+VALUES (7, 7);
+
 
 -- Insert address test account
 INSERT INTO addresses (floor, apt, area, city, county, building_num, street, created_by)
@@ -59,3 +66,66 @@ VALUES
     (2, 5, 'Downtown', 'Amsterdam', 'Noord-Holland', '12B', 'Main Street', 1, 1),
     (3, 6, 'Uptown', 'Berlin', 'Berlin', '34C', 'Park Ave', 2, 2),
     (1, 4, 'Midtown', 'Paris', 'Paris', '78D', 'Rue St', 1, 3);
+
+--insert new properties 
+INSERT INTO properties (
+    description,
+    show_inst,
+    price,
+    property_type,
+    bedrooms,
+    bathrooms,
+    property_realtor_commission,
+    buyer_realtor_commission,
+    area_space,
+    year_built,
+    latitude,
+    longitude,
+    status,
+    exp_date,
+    created_by,
+    owner_id,
+    images_urls,
+    mls_num
+) VALUES
+(
+    'شقة حلوة مع إطلالة على المدينة',
+    'اتصل قبل بيوم لعرض العقار',
+    150000.00,
+    'apartment',
+    3,
+    2,
+    2.5,
+    2.0,
+    120,
+    2015,
+    34.7306,
+    36.7097,
+    'active',
+    '2026-01-01',
+    1,   -- user_id موجود بجدول users
+    2,   -- user_id موجود بجدول users
+    null,
+    123456
+),
+(
+    'فيلا واسعة مع حديقة وبركة سباحة',
+    'مفتوح طول الأسبوع',
+    750000.00,
+    'villa',
+    5,
+    4,
+    5.0,
+    4.5,
+    600,
+    2020,
+    34.7320,
+    36.7100,
+    'active',
+    '2026-05-15',
+    2,
+    2,
+    null,
+    789012
+);
+
