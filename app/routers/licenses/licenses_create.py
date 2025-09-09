1a7e3058ba143f735b540a9cb730fcc7cd35bbfd
ayla <alraasayla@gmail.com>
2025-09-09 15:52:51 +0300

    generate the license number and fixing the status and type enum

--- app/routers/licenses/licenses_create.py
+++ app/routers/licenses/licenses_create.py
@@ -1,26 +1,35 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 from fastapi import Form
-
+from typing import Dict, Any
+from .license_type_enum import LicenseType
+from .license_status_enum import LicenseStatus
 class LicenseCreate(BaseModel):
-    lic_num: int = Field(..., gt=0, lt=100001)
-    lic_status: str
-    lic_type: str
+    lic_status: Dict[str, Any]
+    lic_type: Dict[str, Any]
     user_id: int = Field(..., gt=0)
     agency_id: int = Field(..., gt=0)
 
     @classmethod
     def as_form(
         cls,
-        lic_num: int = Form(...),
         lic_status: str = Form(...),
         lic_type: str = Form(...),
         user_id: int = Form(...),
         agency_id: int = Form(...)
     ):
         return cls(
-            lic_num=lic_num,
             lic_status=lic_status,
             lic_type=lic_type,
             user_id=user_id,
             agency_id=agency_id
         )
+
+    @field_validator("lic_status")
+    def validate_staus(cls, value):
+        LicenseStatus.validate_dict(value)
+        return value
+    
+    @field_validator("lic_type")
+    def validate_type(cls, value):
+        LicenseType.validate_dict(value)
+        return value
--- app/routers/licenses/licenses_out.py
+++ app/routers/licenses/licenses_out.py
@@ -1,12 +1,12 @@
 from pydantic import BaseModel
 
-from typing import Optional
+from typing import Optional, Dict, Any
 
 class LicenseOut(BaseModel):
     license_id: Optional[int]
-    lic_num: Optional[int]
-    lic_status: Optional[str]
-    lic_type: Optional[str]
+    lic_num: Optional[str]
+    lic_status: Optional[Dict[str, Any]]
+    lic_type: Optional[Dict[str, Any]]
     
     user: Optional[object]
     agency: Optional[object]
